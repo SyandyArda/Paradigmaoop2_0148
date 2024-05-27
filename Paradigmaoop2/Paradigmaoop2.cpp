f93e2f82e8274a983d6ebbefded906299f004819
SyandyArda <syandi.2901@gmail.com>
2024-05-27 10:06:50 +0700

    commit tiga

--- Paradigmaoop2/Paradigmaoop2.cpp
+++ Paradigmaoop2/Paradigmaoop2.cpp
@@ -16,5 +16,26 @@ public:
 	}
 };
 
-class lia
+class lia :public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari lia" << endl;
+	}
+
+};
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang::pesan();
+
+	return 0;
+}
+
 
